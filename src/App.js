b82a72a07666072e906f176359148cb3cbaa6fdd
Remilekun Salami <remilekun.salami@gmail.com>
2020-12-09 18:41:51 +0100

    feat: splash screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import RNBootSplash from 'react-native-bootsplash';
 import { ThemeProvider } from 'styled-components';
 import Router from '@router';
 import theme from '@theme';
 
 const App = () => {
+  useEffect(() => {
+    (async () => {
+      await RNBootSplash.hide({ duration: 250, fade: true });
+    })();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
